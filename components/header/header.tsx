086ab25dce4b5c6373136889fc4665380cc41a90
Đăng Khoa <khoavd@voyager-hcm.com>
2024-09-05 22:07:57 +0700

    first commit

---
+++ components/header/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="bg-slate-900 text-slate-200 h-14">
+      <div className="px-6 flex justify-between">
+        <div>Logo</div>
+        <div>Form</div>
+      </div>
+    </div>
+  )
+}
